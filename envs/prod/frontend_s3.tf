e43ad0a0382562a821fd73b6b96a21928a872d5c
Alirezabg <alirezabg@gmail.com>
2025-10-08 15:17:02 +0100

    create a s3 for frontend

--- envs/prod/frontend_s3.tf
+++ envs/prod/frontend_s3.tf
@@ -1,7 +1,12 @@
-variable "project" { type = string }
-variable "region"  { type = string  default = "eu-west-2" }
+variable "project" {
+  type = string
+}
 
-# Public website bucket for quick testing (no CloudFront, no HTTPS)
+variable "region" {
+  type    = string
+  default = "eu-west-2"
+}
+
 resource "aws_s3_bucket" "frontend_web" {
   bucket = "${var.project}-frontend-web-${var.region}"
 }
@@ -16,32 +21,41 @@ resource "aws_s3_bucket_public_access_block" "frontend_web" {
 
 resource "aws_s3_bucket_ownership_controls" "frontend_web" {
   bucket = aws_s3_bucket.frontend_web.id
-  rule { object_ownership = "ObjectWriter" }
+  rule {
+    object_ownership = "ObjectWriter"
+  }
 }
 
-# Enable website hosting (note: this endpoint is HTTP-only)
 resource "aws_s3_bucket_website_configuration" "frontend_web" {
   bucket = aws_s3_bucket.frontend_web.id
 
-  index_document { suffix = "index.html" }
-  error_document { key    = "index.html" } # SPA fallback
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
 }
 
-# Public read policy (objects only). Testing only.
 data "aws_iam_policy_document" "public_read" {
   statement {
     sid     = "PublicReadGetObject"
     effect  = "Allow"
     actions = ["s3:GetObject"]
-    principals { type = "AWS", identifiers = ["*"] }
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
     resources = ["${aws_s3_bucket.frontend_web.arn}/*"]
   }
 }
 
 resource "aws_s3_bucket_policy" "frontend_web" {
-  bucket = aws_s3_bucket.frontend_web.id
-  policy = data.aws_iam_policy_document.public_read.json
-
+  bucket     = aws_s3_bucket.frontend_web.id
+  policy     = data.aws_iam_policy_document.public_read.json
   depends_on = [aws_s3_bucket_public_access_block.frontend_web]
 }
 
@@ -49,7 +63,6 @@ output "frontend_bucket_name" {
   value = aws_s3_bucket.frontend_web.id
 }
 
-# The public website URL you can open in a browser (http)
 output "frontend_website_url" {
   value = aws_s3_bucket_website_configuration.frontend_web.website_endpoint
 }
