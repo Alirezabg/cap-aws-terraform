07674651f315f5c802e5a769eed7704d03c77137
Alirezabg <alirezabg@gmail.com>
2025-10-08 15:58:34 +0100

    add iam for fronend

--- envs/prod/frontend_iam.tf
+++ envs/prod/frontend_iam.tf
@@ -1,3 +1,30 @@
+
+data "aws_iam_openid_connect_provider" "github" {
+  arn = "arn:aws:iam::<YOUR_ACCOUNT_ID>:oidc-provider/token.actions.githubusercontent.com"
+}
+
+resource "aws_iam_role" "github_actions_deploy" {
+  name = "github-actions-deploy-frontend-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = data.aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:batuncer/OnlineShop-Frontend:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy" "frontend_s3_deploy" {
   name = "frontend-s3-deploy"
   role = aws_iam_role.github_actions_deploy.id
@@ -22,31 +49,6 @@ resource "aws_iam_role_policy" "frontend_s3_deploy" {
   })
 }
 
-resource "aws_iam_role" "github_actions_deploy" {
-  name = "github-actions-deploy-frontend-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Federated = aws_iam_openid_connect_provider.github.arn
-        }
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Condition = {
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:batuncer/OnlineShop-Frontend:ref:refs/heads/main"
-          }
-        }
-      }
-    ]
-  })
+output "github_actions_deploy_role_arn" {
+  value = aws_iam_role.github_actions_deploy.arn
 }
-
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
-}
-
