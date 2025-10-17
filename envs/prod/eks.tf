18c79dcb93e982f8f926ac5e0d231e09598551e5
abagheri <alirezabg@gmail.com>
2025-10-17 13:05:45 +0100

    change nat gateway

--- envs/prod/eks.tf
+++ envs/prod/eks.tf
@@ -17,8 +17,8 @@ module "vpc" {
     "10.10.32.0/19",
   ]
 
-  enable_nat_gateway = false
-  single_nat_gateway = false
+  enable_nat_gateway = true
+  single_nat_gateway = true  
 
   enable_dns_hostnames = true
   enable_dns_support   = true
