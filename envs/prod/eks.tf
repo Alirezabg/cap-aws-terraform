e3cd74fe7c91738ad9786f3c5d356cc6351a9b4a
abagheri <alirezabg@gmail.com>
2025-10-17 13:56:35 +0100

    private up

--- envs/prod/eks.tf
+++ envs/prod/eks.tf
@@ -16,14 +16,15 @@ module "vpc" {
     "10.10.0.0/19",
     "10.10.32.0/19",
   ]
+  map_public_ip_on_launch = true
   private_subnets = [   
   "10.10.64.0/19",
   "10.10.96.0/19",
 ]
 
 
-  enable_nat_gateway = true
-  single_nat_gateway = true  
+  enable_nat_gateway = false
+  single_nat_gateway = false  
 
   enable_dns_hostnames = true
   enable_dns_support   = true
